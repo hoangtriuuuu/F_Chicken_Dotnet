f65ed7e7981c7592c93fc490727f3502276580bf
hoangtrieu <hoangtrieu2505@gmail.com>
2026-03-22 15:45:12 +0700

    Fix Dockerfile at root level

--- FChicken.API/obj/Debug/net10.0/FChicken.API.AssemblyInfo.cs
+++ FChicken.API/obj/Debug/net10.0/FChicken.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FChicken.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcaa033612a2774e176ff562099526911e699e1f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b12c88401283c560200f4e65905a4c8abdbe8ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("FChicken.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FChicken.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
